70596985cff3dab4d6f9f4aa9dceb5ed9e88c42f
jeiel2013 <contatojeiel2013@gmail.com>
2026-01-23 00:05:03 -0300

    Fix vite.config esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
-import { defineConfig, type TerserOptions } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
+
   build: {
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
-          'iconify': ['@iconify/react'],
-        }
-      }
-    },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true, // Remove console.logs em produção
-        drop_debugger: true,
+          iconify: ['@iconify/react'],
+        },
       },
-    } as TerserOptions,
-  }
+    },
+  },
 })
